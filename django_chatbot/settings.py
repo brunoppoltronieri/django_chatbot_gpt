531376bb0da8a19387ae50cc3824736881df4dac
Bruno Poltronieri <bppoltronieri@ucs.br>
2023-10-30 14:44:56 -0300

    alteração na versao do frozenlist

--- django_chatbot/settings.py
+++ django_chatbot/settings.py
@@ -80,11 +80,14 @@ WSGI_APPLICATION = 'django_chatbot.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'chatbot_db_kcyc',
+        'USER': 'admin',
+        'PASSWORD': 'LYrkNVEsvASpw0jzgCY0VvShwWAthi7J',
+        'HOST': 'dpg-ckvtft6b0mos73cmenu0-a.oregon-postgres.render.com',
+        'PORT': '5432',
     }
 }
-
 
 
 # Password validation
